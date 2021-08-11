6f459a5e6284692499cd1faf1aeeb7277c1f865d
Natalie Barba <nbarba2010@gmail.com>
2021-08-11 15:14:49 -0700

    commit tree c284efcc45cb985e9349cad7bd0c978a7c328041

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driversArray, name) {
+    return driversArray.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(driversArray, firstLetters) {
+    return driversArray.filter(driver => driver.startsWith(firstLetters));
+}
+
+function matchName(driverObjects, nameMatch) {
+    return driverObjects.filter(driver => driver.name === nameMatch);
+}
